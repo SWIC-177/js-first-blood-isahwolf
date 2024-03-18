49e3479edf72e8d6e342805fc10ac67f0161be9f
isahwolf <isaiah.wolf@student.swic.edu>
2024-03-18 15:51:45 -0500

    Added comments

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-let name = "Isaiah Wolf";
-let age = 19;
+let name = "Isaiah Wolf"; //string operator
+let age = 19; //number operator
 
 
-function verifyAdulthood(name, age) 
+function verifyAdulthood(name, age) //fucntion with parameters
 {
-    if (age >= 18) 
+    if (age >= 18) //if older than 18
     {
         return `Welcome, ${name}!`;
     } 
-    else 
+    else //if younger than 18
     {
         return `You are not old enough to enter, ${name}.`;
     }
 }
 
 
-console.log(verifyAdulthood(name, age));
+console.log(verifyAdulthood(name, age)); //displays text based on which if else statement was met
