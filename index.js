79f30bb0b5e55ec48cdada14f51558caadf7a88a
Manav Misra <manavm@visionwebsoft.com>
2024-03-22 17:55:31 -0500

    Refactor code to use const and update function parameters

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-let name = "Isaiah Wolf"; //string operator
-let age = 19; //number operator
+const fullName = "Isaiah Wolf"; // string operator
+const personAge = 19; // number operator
 
+function verifyAdulthood(name, age) {
+  // function with parameters
+  if (age >= 18) {
+    // if older than 18
+    return `Welcome, ${name}!`;
+  }
 
-function verifyAdulthood(name, age) //fucntion with parameters
-{
-    if (age >= 18) //if older than 18
-    {
-        return `Welcome, ${name}!`;
-    } 
-    else //if younger than 18
-    {
-        return `You are not old enough to enter, ${name}.`;
-    }
+  return `You are not old enough to enter, ${name}.`;
 }
 
-
-console.log(verifyAdulthood(name, age)); //displays text based on which if else statement was met
+console.log(verifyAdulthood(fullName, personAge));
