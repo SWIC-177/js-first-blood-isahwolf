bff7aa3cdf6f5ebba67e197327ca0a3672da0a32
isahwolf <isaiah.wolf@student.swic.edu>
2024-03-27 11:47:00 -0500

    fizzbuzz while loop

--- fizzbuz.js
+++ fizzbuz.js
@@ -9,3 +9,16 @@ for (let i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+let num = 1;
+while (num <= 100) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (num % 3 === 0) {
+        console.log("Fizz");
+    } else if (num % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(num);
+    }
+    num++;
+}
