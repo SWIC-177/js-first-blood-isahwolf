62e6066768d72df5ff2710de189bc867f4818c21
isahwolf <isaiah.wolf@student.swic.edu>
2024-03-27 12:24:11 -0500

    Age verify code fix (changed userName and userAge)

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-let name = "Isaiah Wolf"; //string operator
-let age = 19; //number operator
-
-
-function verifyAdulthood(name, age) //fucntion with parameters
-{
-    if (age >= 18) //if older than 18
-    {
-        return `Welcome, ${name}!`;
-    } 
-    else //if younger than 18
-    {
-        return `You are not old enough to enter, ${name}.`;
-    }
-}
-
-
-console.log(verifyAdulthood(name, age)); //displays text based on which if else statement was met
+const person = {
+    name: "John",
+    age: 17,
+  };
+  
+  function verifyAdulthood(personName, personAge) {
+    if (personAge >= 18) return `Welcome, ${personName}!`;
+  
+    return `You are not old enough to enter, ${personName}.`;
+  }
+  
+  const adultMessage = verifyAdulthood(person.name, person.age);
+  
+  console.log(adultMessage);
+  
