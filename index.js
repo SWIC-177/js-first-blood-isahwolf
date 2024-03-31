7c2f9458e34317fba8d8da5a474362b2986d4d20
Manav Misra <manavm@visionwebsoft.com>
2024-03-30 19:55:01 -0500

    fix: resolve merge conflicts

--- index.js
+++ index.js
@@ -1,52 +1,33 @@
-<<<<<<< HEAD
 const people = [
-    {
-      name: "John",
-      age: 17,
-    },
-    {
-      name: "Jane",
-      age: 20,
-    },
-    {
-      name: "Mary",
-      age: 25,
-    },
-    {
-      name: "Peter",
-      age: 30,
-    },
-    {
-      name: "Paul",
-      age: 35,
-    },
-  ];
-  
-  
-  function verifyAdulthood(personName, personAge) {
-    if (personAge >= 18) return `Welcome, ${personName}!`;
-  
-    return `You are not old enough to enter, ${personName}.`;
-  }
-  
-  for (let person of people) 
   {
-    const adultMessage = verifyAdulthood(person.name, person.age);
-    console.log(adultMessage);
-  }
-=======
-const fullName = "Isaiah Wolf"; // string operator
-const personAge = 19; // number operator
+    name: "John",
+    age: 17,
+  },
+  {
+    name: "Jane",
+    age: 20,
+  },
+  {
+    name: "Mary",
+    age: 25,
+  },
+  {
+    name: "Peter",
+    age: 30,
+  },
+  {
+    name: "Paul",
+    age: 35,
+  },
+];
 
-function verifyAdulthood(name, age) {
-  // function with parameters
-  if (age >= 18) {
-    // if older than 18
-    return `Welcome, ${name}!`;
-  }
+function verifyAdulthood(personName, personAge) {
+  if (personAge >= 18) return `Welcome, ${personName}!`;
 
-  return `You are not old enough to enter, ${name}.`;
+  return `You are not old enough to enter, ${personName}.`;
 }
 
-console.log(verifyAdulthood(fullName, personAge));
->>>>>>> 2fe609365c29c8368eb3ab198f65c6345a403d42
+for (let person of people) {
+  const adultMessage = verifyAdulthood(person.name, person.age);
+  console.log(adultMessage);
+}
