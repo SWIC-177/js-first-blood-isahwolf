df8f2b743337397f718a4da43edad4f789b7bd97
isahwolf <isaiah.wolf@student.swic.edu>
2024-04-09 16:52:59 -0500

    fix errors for 2nd part of tuesdays hw

--- index.js
+++ index.js
@@ -16,3 +16,48 @@ otherPerson.name = "Jane Doe";
 
 console.log(`Person's name is: ${person.name}`);
 console.log(`Other Person's name is: ${otherPerson.name}`);
+
+const people = [
+  {
+    id: 1,
+    name: "John Doe",
+    age: 23,
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    age: 29,
+  },
+  {
+    id: 3,
+    name: "Jim Doe",
+    age: 34,
+  },
+  {
+    id: 4,
+    name: "Jill Doe",
+    age: 45,
+  },
+  {
+    id: 5,
+    name: "Jack Doe",
+    age: 56,
+  },
+  {
+    id: 6,
+    name: "Jenny Doe",
+    age: 67,
+  },
+];
+
+function renameOdds(p) {
+  for (let i = 0; i < people.length; i += 1) {
+    if (p[i].id % 2 !== 0) {
+      people[i].name = "Odd Name";
+    }
+  }
+
+  return p;
+}
+
+console.log(renameOdds(people));
